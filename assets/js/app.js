c59deae55693993294ed2e14ac9b138a9b3540c9
Somiya Rani <somiya1093@gmail.com>
2025-02-24 02:10:21 -0500

    - Redesigned website theme and layout. - Added new experience and skills section. - Added new Genie (Coming Soon) sticky link. - Added wave background. - Updated projects images. - Updated about layout and content. - Updated footer copyright and added social links. - Removed dark and light theme. - Removed light theme github logo.

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,76 +25,9 @@ for (let i = 0; i < navItems.length; ++i) {
 }
 }
 
-const storageKey = 'theme-preference'
-
-const getColorPreference = () => {
-    if (localStorage.getItem(storageKey))
-      return localStorage.getItem(storageKey)
-    else
-      return window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'light'
-}
-
-const setPreference = () => {
-    localStorage.setItem(storageKey, theme.value)
-    reflectPreference()
-  }
-  
-const reflectPreference = () => {
-    document.firstElementChild
-      .setAttribute('data-theme', theme.value)
-
-    document
-      .querySelector('#theme-toggle')
-      ?.setAttribute('aria-label', `${theme.value}theme`)
-
-    updateThemeElement()
-}
-
-const theme = {
-    value: getColorPreference(),
-}
-
-const onClick = () => {
-    if (theme.value === 'light') {
-        theme.value = 'dark'
-    } else {
-        theme.value = 'light'
-    }
-  
-    setPreference()
-  }
-
-const updateThemeElement = () => {
-  document
-  .querySelectorAll('.github-logo')
-  .forEach(function(currentValue, currentIndex, listObj) {
-    currentValue.src = (theme.value === 'dark')
-    ? '/assets/images/icons/github-logo-white.webp'
-    : '/assets/images/icons/github-logo.webp'
-      },);
-}
-
 window.onload = () => {
-    // set on load so screen readers can see latest value on the button
-    reflectPreference()
-  
-    // now this script can find and listen for clicks on the control
-    document
-      .querySelector('#theme-toggle')
-      .addEventListener('click', onClick)
-
     setActiveNavItem()
 }
-
-// sync with system changes
-window
-  .matchMedia('(prefers-color-scheme: dark)')
-  .addEventListener('change', ({matches:isDark}) => {
-    theme.value = isDark ? 'dark' : 'light'
-    setPreference()
-  })
 
 const sections = document.getElementsByTagName('section')
 const scrollOffsetTop = sections[0].offsetTop
